4a155a88c1d5ca8e238246f6730f8dff4c703325
Burak Güzey <burakguzey20@gmail.com>
2023-12-20 23:50:46 +0300

    Creating browser

---
+++ Project 4/Project 4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project 4
+//
+//  Created by Burak Güzey on 20.12.2023.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+}
+
